0fa475ca601055de0e9ef5659fdc8d2062a0d7f6
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-07 22:43:20 +0100

    add registration Form, add BrowserRouter

---
+++ frontend/src/component/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,37 @@
+import {ChangeEvent, FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+export default function RegistrationForm() {
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+
+    const navigate = useNavigate()
+
+    function handleUsername(event: ChangeEvent<HTMLFormElement>) {
+        setUsername(event.target.value)
+    }
+    function handlePassword(event: ChangeEvent<HTMLFormElement>) {
+        setPassword(event.target.value)
+    }
+    function handleSubmit (event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        axios.post("api/users/", {username, password})
+            .then(navigate ("/"))
+            .catch(e => console.error(e))
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username
+                <input type="text" value={username} onChange={handleUsername}/>
+            </label>
+            <label>
+                Password
+                <input type="password" value={password} onChange={handlePassword}/>
+            </label>
+            <button type="submit">Sign Up</button>
+        </form>
+    )
+}
